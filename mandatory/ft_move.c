3f2e956cc34c35d451e38d679e8c78c7e24f1a9e
gala <galaad.mersch@gmail.com>
2024-08-29 01:23:09 +0200

    texture is set, but not all

--- mandatory/ft_move.c
+++ mandatory/ft_move.c
@@ -6,7 +6,7 @@
 /*   By: gmersch <gmersch@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/15 20:01:46 by gmersch           #+#    #+#             */
-/*   Updated: 2024/08/24 16:21:34 by gmersch          ###   ########.fr       */
+/*   Updated: 2024/08/28 23:54:12 by gmersch          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,12 +28,12 @@ void	ft_move_hook(mlx_key_data_t keydata, t_player *p)
 	//move front
 	if (keydata.key == MLX_KEY_W && (keydata.action == MLX_PRESS || keydata.action == MLX_REPEAT))
 	{
-		printf("input = %d\n", p->player_move_f == true);
+		//printf("input = %d\n", p->player_move_f == true);
 		p->player_move_f = true;
-	}	
+	}
 	else if (keydata.key == MLX_KEY_W && keydata.action == MLX_RELEASE && keydata.action != MLX_REPEAT)
 	{
-		printf("test\n");
+		//printf("test\n");
 		p->player_move_f = false;
 	}
 
@@ -60,15 +60,15 @@ void	ft_move_wasd(t_player *p)
 {
 	int			newPosX;
 	int			newPosY;
-	
-	char map[7][11] = {"1111111111\0",	
+
+	char map[7][11] = {"1111111111\0",
 					   "1000100011\0",
 					   "1000000001\0",
 					   "1000000001\0",
 					   "1000000001\0",// le perso est au deuxieme 0 de cette ligne et regarde vers le nord
 					   "1111111111\0",
 					   ""};
-	
+
 	//move front
 	if (p->player_move_f)
 	{
--- mandatory/ft_ray_casting.c
+++ mandatory/ft_ray_casting.c
@@ -6,15 +6,15 @@
 /*   By: gmersch <gmersch@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/14 15:25:10 by gmersch           #+#    #+#             */
-/*   Updated: 2024/08/28 20:10:59 by gmersch          ###   ########.fr       */
+/*   Updated: 2024/08/29 01:21:01 by gmersch          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cube.h"
 
-static void	ft_print_ray(t_player *p, int ex)
+static void	ft_print_ray(t_player *p, int sx)
 {
-	int i;
+	int sy;
 	int texture_y;
 	int texture_x;
 	int pixel;
@@ -25,7 +25,7 @@ static void	ft_print_ray(t_player *p, int ex)
 	if (p->rc->side)
 		color = (color / 2) | 0xFF;
 
-	
+
 	if (p->rc->side == 0) // Mur vertical (nord-sud)
 		wall_hit_position = p->posY + p->rc->perpWallDist * p->rc->rayDirY;
 	else // Mur horizontal (est-ouest)
@@ -35,76 +35,67 @@ static void	ft_print_ray(t_player *p, int ex)
 	texture_x = (wall_hit_position * (float)(p->game->north_texture->width - 1));
 	if(p->rc->side == 0 && p->rc->rayDirX > 0.0)
 		texture_x = p->game->north_texture->width - texture_x - 1;
-    if(p->rc->side == 1 && p->rc->rayDirY < 0.0)
+	if(p->rc->side == 1 && p->rc->rayDirY < 0.0)
 		texture_x = p->game->north_texture->width - texture_x - 1;
-	
+
 	if (texture_x < 0)
 		texture_x = 0;
 	if (texture_x >= p->game->north_texture->width)
 		texture_x = p->game->north_texture->width - 1;
 
-	i = 0;
-	while (i < p->rc->drawStart)
+	sy = 0;
+	while (sy < p->rc->drawStart)
 	{
-		mlx_put_pixel(p->game->image, ex, i, 0x0000B4FF); // Couleur du ciel
-		i++;
+		mlx_put_pixel(p->game->image, sx, sy, 0x0000B4FF); // Couleur du ciel
+		sy++;
 	}
-	while (i < p->rc->drawEnd)
+	while (sy < p->rc->drawEnd)
 	{
 		// Calculer la position Y sur la texture en fonction de la hauteur du mur
-		texture_y = (int)((i - p->rc->drawStart) * (p->game->north_texture->height / (p->rc->drawEnd - p->rc->drawStart)));
+		texture_y = (int)((sy - p->rc->drawStart) * /*(float)*/p->game->north_texture->height / (p->rc->drawEnd - p->rc->drawStart));
 		if (texture_y < 0)
 			texture_y = 0;
 		if (texture_y >= p->game->north_texture->height)
 			texture_y = p->game->north_texture->height - 1; //if point is too big or too small : resize to smaller point or bigger from the texture.
 
-		//Because pixel is store in a ray9* and not a ray of ray (**), we do :
+		//Because pixel is store in a char* and not a ray of ray (char**), we do :
 		//texture_y (wich is the y of pixel we want to put)
 		// * the weight of texture : like 6 * 64 (and the result is the line we want)
 		//AND we add texture x for being not at the start of the line, but in the correct x pos.
 		//Put it on escalidraw or come see me (or discord) if explaination needed
 		//so it gives us this line :
-		
-		//pixel = ((texture_y * p->game->north_texture->width) + texture_x) * 4;
-		pixel = p->game->north_texture->width * texture_y * 4 + abs(texture_x - (int)p->game->north_texture->width + 1) * 4;
 
-		//verif if ok
+
+		//pixel need to be multiplicate by 4, because pixel is a group of 4 pixel (R + G + B + A)
+		pixel = (p->game->north_texture->width * texture_y + texture_x) * 4;
+
+		//verif if undo 0
 		if (pixel < 0)
 			pixel = 0;
-		//if (pixel >= p->game->north_texture->width * p->game->north_texture->height)
-		//	pixel = p->game->north_texture->width * p->game->north_texture->height - 1;
-		//printf("%d = pixel\n", pixel);
-		//printf("%d = width\n", p->game->north_texture->width);
-		//printf("%d = height\n", p->game->north_texture->height);
-		//and now that we have the index of the pixel in our ray, lets get the color of it :
-		//color = p->game->north_texture->pixels[pixel];
-		// color = ((u_int16_t)p->game->north_texture->pixels[pixel] << 24) |
-        //            (u_int16_t)(p->game->north_texture->pixels[pixel+ 1] << 16) |
-        //            (u_int16_t)(p->game->north_texture->pixels[pixel + 2] << 8) |
-        //         	(u_int16_t)p->game->north_texture->pixels[pixel + 3];
 
-		color = p->game->north_texture->pixels[pixel];
+		//and now that we have the index of the pixel in our ray, lets get the color of it :
+
+		color = *(uint32_t*)(p->game->north_texture->pixels + pixel);
+		//now the color is set to the R (red). we need now to add to him GBA, by increment the adress of pixel one by one :
+		//pixel is a uint8_t, and color is a uint32_t. So we add 8 bites each time we whant to add a color
+			//NOTE : color is like this : 00(RED) 00(GREEN) 00(BLUE) 00(transparence but idk in english)
+			//so color is = to 00000000 in hexa, and you add here the color :
 		color = (color << 8) + p->game->north_texture->pixels[pixel+ 1];
 		color = (color << 8) + p->game->north_texture->pixels[pixel+ 2];
 		color = (color << 8) + p->game->north_texture->pixels[pixel+ 3];
+		//up here we move to 8 bites the bites of color to the letf, then we add the G, then we move again to 8, add B etc..
+		//if not clear come see me or in discord (galadou)
 
-		//p->game->north_texture->pixels
-		//printf("%08X = hexa\n", color);
-		//printf("%X = hexa1\n", p->game->north_texture->pixels[pixel]);
-		//printf("%X = hex2a\n", (p->game->north_texture->pixels[pixel+ 1]));
-		//printf("%X = hexa3\n", p->game->north_texture->pixels[pixel + 2] << 2);
-		//printf("%X = hexa4\n", p->game->north_texture->pixels[pixel + 3]);
-		//printf("%d = pixel\n", pixel);
 
-		// Afficher le pixel sur l'image finale
-		mlx_put_pixel(p->game->image, ex, i, color);
-		i++;
+		//Add pixel to the image : (sx is screen x, sy is screen y)
+		mlx_put_pixel(p->game->image, sx, sy, color);
+		sy++;
 	}
 	// Afficher le sol en dessous du mur
-	while (i < p->game->height)
+	while (sy < p->game->height)
 	{
-		mlx_put_pixel(p->game->image, ex, i, 0x000000FF / 2); // Couleur du sol
-		i++;
+		mlx_put_pixel(p->game->image, sx, sy, 0x000000FF / 2); // Couleur du sol
+		sy++;
 	}
 }
 
@@ -160,7 +151,7 @@ void	ft_ray_casting(void *param)
 	//ex is like ecran x (horizontal value of pixel of the screen)
 
 	t_player *p;
-	int	ex;
+	int	sx; //screen x
 	char map[7][11] = {"1111111111\0",
 					"1000100011\0",
 					"1000000001\0",
@@ -170,13 +161,13 @@ void	ft_ray_casting(void *param)
 					""};
 
 	p = (t_player *)param;
-	
+
 	struct timeval time; // add fps counter
 	suseconds_t usec;
 	time_t sec;
 	char *fps;
 
-	ex = 0;
+	sx = 0;
 
 	if (p->game->pause)
 		return ;
@@ -185,10 +176,10 @@ void	ft_ray_casting(void *param)
 	sec = time.tv_sec;
 
 	ft_move_wasd(p);//ici;
-	while (ex < p->game->width)
+	while (sx < p->game->width)
 	{
 		//we redefine rc every frame, because it set var needed by ray casting
-		ft_define_rc(p, ex);
+		ft_define_rc(p, sx);
 		//know what side of the wall we'r talking
 		ft_find_side(p, map);
 		//calcul wall height and remove fishy
@@ -196,8 +187,8 @@ void	ft_ray_casting(void *param)
 		//define draw start and draw end
 		ft_define_print(p);
 		//print ray, its here to change color :
-		ft_print_ray(p, ex);
-		ex++;
+		ft_print_ray(p, sx);
+		sx++;
 	}
 	//print fps
 	gettimeofday(&time, NULL);
